955b452124a3525da51d05f1b3aa79af7b0163ad
Akshay Bhardwaj <akshaybhardwaj96.ab@gmail.com>
2024-01-05 22:33:52 +0530

    Show loading skeletons

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,20 @@
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+import React from "react";
+
+const GameCardSkeleton = () => {
+  return (
+    <Card>
+      <Skeleton
+        width={"300px"}
+        height={"200px"}
+        borderRadius={10}
+        overflow={"hidden"}
+      ></Skeleton>
+      <CardBody>
+        <SkeletonText></SkeletonText>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCardSkeleton;
